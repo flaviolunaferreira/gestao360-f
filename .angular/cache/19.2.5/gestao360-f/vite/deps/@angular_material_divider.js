abce35f255a1045c52229afbf95dc75d8332f6fa
theCoyote <flaviolunaferreira@gmail.com>
2025-03-26 12:04:57 -0300

    salvando alterações

---
+++ .angular/cache/19.2.5/gestao360-f/vite/deps/@angular_material_divider.js
@@ -0,0 +1,17 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-VWKRQNNL.js";
+import "./chunk-QEGPYYQA.js";
+import "./chunk-WL5COFDT.js";
+import "./chunk-M3HR6BUY.js";
+import "./chunk-H5HPXNXS.js";
+import "./chunk-FC7CZYYG.js";
+import "./chunk-4DE4OFR5.js";
+import "./chunk-WNVI4JOA.js";
+import "./chunk-56PPHEMJ.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
