ce85e107c089f2465f10f3c82216b5a38df6a682
theCoyote <flaviolunaferreira@gmail.com>
2025-03-26 08:43:45 -0300

    first commit

---
+++ .angular/cache/19.2.4/gestao360-f/vite/deps/@angular_material_divider.js
@@ -0,0 +1,17 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-KQNVAVO4.js";
+import "./chunk-7CA54YE4.js";
+import "./chunk-WT5CZTQC.js";
+import "./chunk-M3HR6BUY.js";
+import "./chunk-H5HPXNXS.js";
+import "./chunk-FC7CZYYG.js";
+import "./chunk-VEE34BE5.js";
+import "./chunk-WNVI4JOA.js";
+import "./chunk-56PPHEMJ.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
